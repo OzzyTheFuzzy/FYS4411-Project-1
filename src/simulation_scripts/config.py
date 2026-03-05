c8048d2361d5ac35bc96a11f47c83281e5169c06
OzzyTheFuzzy <oskarfa@uio.no>
2026-03-05 20:52:51 +0100

    restructured the whole program so train and train steepest descent have a helper program to avoid unecessary code

--- src/simulation_scripts/config.py
+++ src/simulation_scripts/config.py
@@ -3,13 +3,13 @@ import numpy as np
 
 output_filename = "../data/vmc_playground.csv"
 
-nparticles = 500
-dim = 3
+nparticles = 1
+dim = 1
 nsamples = int(2**12)  # 2**18 = 262144
 nchains = 1 # number of Markov chains. When you parallelize, you can set this to the number of cores. Note you will have to implement this yourself.
 eta = 0.01
 training_cycles = 10_000  # this is cycles for the ansatz
-mcmc_alg = "langevin" # "metropolis" or "langevin"
+mcmc_alg = "metropolis" # "metropolis" or "langevin"
 scale = 0.9
 backend = "torch"
 optimizer = "gd"
