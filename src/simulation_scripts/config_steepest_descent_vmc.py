945a00f8f7215e3430194bc9183db24464f5a95e
OzzyTheFuzzy <oskarfa@uio.no>
2026-03-10 22:01:34 +0100

    ran tests. Stuff works

--- src/simulation_scripts/config_steepest_descent_vmc.py
+++ src/simulation_scripts/config_steepest_descent_vmc.py
@@ -5,12 +5,12 @@ output_filename = "../../data/vmc_vs_exact_test.txt"
 
 nsamples = int(2**15)   # samples for the final calculation of the energy after training
 nchains = 1
-eta = 0.12 # learning rate for steepest descent optimizer
-training_cycles = 10_000 # number of training cycles for each alpha 
+eta = 0.02 # learning rate for steepest descent optimizer
+training_cycles = 125_000 # number of training cycles for each alpha 
 num_iterations = 50 # number of different alphas
 mcmc_alg = "langevin" # either user metropolis or langevin 
 backend = "torch"
-optimizer = "adam" # either "gd" or "adam"
+optimizer = "gd" # either "gd" or "adam"
 batch_size = 200
 detailed = True
 wf_type = "vmc"
@@ -19,7 +19,7 @@ burn_in = 1 * training_cycles // 10 # number of burn-in samples to discard
 scale = 1.5 # scale for the MCMC proposal distribution (will be tuned with alpha during training)
 
 # Arrays for VMC_vs_exact.py
-alpha_array = np.array([0.9])
+alpha_array = np.array([0.7])
 dim = 1
 nparticles = 100
 omega = 1.0 # harmonic oscillator 
