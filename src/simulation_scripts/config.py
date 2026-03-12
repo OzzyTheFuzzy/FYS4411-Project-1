9673fbbf14309a977ec678e15f7d43c37b2004db
OzzyTheFuzzy <oskarfa@uio.no>
2026-03-12 18:12:21 +0100

    interactions work but something is off with final sampling of the best alpha

--- src/simulation_scripts/config.py
+++ src/simulation_scripts/config.py
@@ -3,7 +3,7 @@ import numpy as np
 
 output_filename = "../data/vmc_playground.csv"
 
-nparticles = 100
+nparticles = 10
 dim = 3
 nsamples = int(10_000)  # 2**18 = 262144
 nchains = 1 # number of Markov chains. When you parallelize, you can set this to the number of cores. Note you will have to implement this yourself.
@@ -19,4 +19,4 @@ wf_type = "vmc"
 seed = 142
 burn_in = training_cycles * 0.2 # number of initial samples to discard as burn-in when training
 alpha_array = np.linspace(0.1, 0.9, 11) # array of alpha values to train on
-a = 0.01     # Jastrow factor strength, set to 0 for no interactions
+a = 0.1     # Jastrow factor strength, set to 0 for no interactions
