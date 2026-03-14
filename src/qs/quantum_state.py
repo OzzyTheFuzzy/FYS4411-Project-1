have fixed the final sampling for interacting case. Found that for alphas that is far away from the true the variance was really high because more particles were really close

src/qs/quantum_state.py
@@ -91,15 +91,23 @@ class QS:
             self.sampler.set_hamiltonian(self.hamiltonian)
             
     def _make_initial_state(self):
+
         # Start positions near origin (Gaussian). Shape: (N, dim)
-        
-        positions = 0.1 * torch.randn(self._N, self._dim, dtype=torch.float64)
+        while True:
+            positions = 0.1 * torch.randn(self._N, self._dim, dtype=torch.float64)
 
+            # safeguard for the hard core condition
+            if self.a != 0.0:
+                r_ij_abs, _ = self.wf.wf.distance_and_distance_vec(positions)
+                iu = torch.triu_indices(self._N, self._N, offset=1)
+                rij = r_ij_abs[iu[0], iu[1]]
+                if torch.any(rij <= self.a):
+                    continue
 
-        # Wavefunction log-probability at positions. self.wf is a VMC object
-        logp = self.wf.wf.log_prob(positions)  
+            # Wavefunction log-probability at positions. self.wf is a VMC object
+            logp = self.wf.wf.log_prob(positions)  
 
-        return State(positions=positions, logp=logp, n_accepted=0, delta=0)
+            return State(positions=positions, logp=logp, n_accepted=0, delta=0)
 
     def set_sampler(self, mcmc_alg, scale=0.1):
 
@@ -139,17 +147,20 @@ class QS:
 
         self._is_initialized()
         self._training_cycles = MC_training_cycles
-
+        self.burn_in = burn_in
         self.alpha_array = alpha_array
         self.mean_num_energies = []
         self.mean_ana_energies = []
-
-        for a in tqdm(alpha_array, desc="[Training progress]", colour="green") if self._log else alpha_array:
-
+        
+        for alpha in tqdm(alpha_array, desc="[Training progress]", colour="green") if self._log else alpha_array:
+            
             print("N, D:", self.hamiltonian._N, self.hamiltonian._dim)
             print("omega_ho:", self.hamiltonian.omega_ho, "omega_z:", self.hamiltonian.omega_z)
 
-            a_tensor = torch.tensor(a, dtype=torch.float64)
+            a_tensor = torch.tensor(alpha, dtype=torch.float64)
+            idx = np.where(alpha_array == alpha)[0].item()
+            self.sampler.rng = np.random.default_rng(self._seed+idx * 17) # use different seed for each alpha to get different samples
+            
 
             # update wavefunction alpha
             self.wf.alpha = a_tensor
@@ -157,13 +168,13 @@ class QS:
 
             # update sampler scale
             if self.mcmc_alg == "metropolis" or self.mcmc_alg == "langevin":
-                self.sampler.scale = self._scale * np.sqrt(1.0 / a)
+                self.sampler.scale = self._scale * np.sqrt(1.0 / alpha)
 
             state= self._make_initial_state() # make initial state for sampling
             # call sample function from sampler class
             E_ana, E_num, _, accept_rate = self.sampler._sample_energy_and_optional_O(
                 self.wf, state,
-                MC_training_cycles, seed=self._seed,
+                MC_training_cycles, seed=self._seed, # use different seed for each alpha to get different samples, but deterministic across runs
                 burn_in=burn_in,
                 need_O=False,
                 num=num,
@@ -178,7 +189,7 @@ class QS:
             self.mean_ana_energies.append(mean_ana_energy)
 
             print(
-                f"alpha={a:.3f} accept_rate={accept_rate:.3f} "
+                f"alpha={alpha:.3f} accept_rate={accept_rate:.3f} "
                 f"mean_E_ana={mean_ana_energy:.6f}, scale={self.sampler.scale:.4f}")
 
         # pick best alpha
@@ -221,7 +232,7 @@ class QS:
 
             E_ana, E_num, O, accept_rate = self.sampler._sample_energy_and_optional_O(
                 self.wf, state,
-                MC_training_cycles, seed=self._seed,
+                MC_training_cycles, seed=self._seed+alpha_j*17,
                 burn_in=burn_in,
                 need_O=True,
                 )
@@ -272,8 +283,18 @@ class QS:
         # Pass burn_in to sampler if set
         if hasattr(self, 'burn_in'):
             self.sampler.burn_in = self.burn_in
-        
-        self._results = self.sampler.sample(self.wf, self._make_initial_state(), nsamples, nchains, seed) # call the sample method from the sampler class, which will perform the sampling and return the results
+            
+        self.sampler.scale = self._scale * np.sqrt(1.0 / self.wf.alpha.item())
+        # call the sample method from the sampler class
+        self._results = self.sampler._sample(
+        wf=self.wf,
+        nsamples=nsamples,
+        state=self._make_initial_state(),
+        scale=self.sampler.scale,
+        seed=seed,
+        chain_id=0,
+        burn_in=self.burn_in if self.burn_in is not None else int(nsamples * 0.2),
+    )
         return self._results
     
 

src/samplers/sampler.py
@@ -22,23 +22,6 @@ class Sampler:
         self.burn_in = None
         self.set_hamiltonian(hamiltonian)
 
-    def sample(self, wf, state, nsamples, nchains=1, seed=None):
-        nchains = check_and_set_nchains(nchains, self._logger)
-        seeds = generate_seed_sequence(seed, nchains)
-        
-        if nchains == 1:
-            chain_id = 0
-            burn_in = self.burn_in if self.burn_in is not None else 0
-            self.results = self._sample(wf, nsamples, state, self.scale, seeds[0], chain_id, burn_in)
-        else:
-            # Parallelize
-            pass
-
-        self._sampling_performed_ = True
-        if self._logger is not None:
-            self._logger.info("Sampling done")
-
-        return self.results
 
 
     def _sample_energy_and_optional_O(self, wf, state, MC_training_cycles,seed, burn_in=0, need_O = False, num= False):
@@ -60,18 +43,16 @@ class Sampler:
             if i < burn_in:
                 continue
 
-            r = state.positions
-
             if num:
-                E_num, E_ana = self.hamiltonian.local_energy(wf, r, num=True)
+                E_num, E_ana = self.hamiltonian.local_energy(wf, state.positions, num=True)
                 E_num_list.append(E_num.detach())
                 E_ana_list.append(E_ana.detach())
             else:
-                E_ana = self.hamiltonian.local_energy(wf, r, num=False)
+                E_ana = self.hamiltonian.local_energy(wf, state.positions, num=False)
                 E_ana_list.append(E_ana.detach())
 
             if need_O:
-                O_val = self.hamiltonian.O_alpha_analytic(wf, r)
+                O_val = self.hamiltonian.O_alpha_analytic(wf, state.positions)
                 O_list.append(O_val.detach())
 
         E_ana = torch.stack(E_ana_list)
@@ -83,61 +64,51 @@ class Sampler:
         return E_ana, E_num, O, accept_rate
 
     def _sample(self, wf, nsamples, state, scale, seed, chain_id, burn_in=0, num=False):
-        """To be called by process. Here the actual sampling is performed."""
+        """
+        Function for final sampling 
+        """
 
-        if self._logger is not None:
-            t_range = tqdm(
-                range(nsamples),
-                desc=f"[Sampling progress] Chain {chain_id+1}",
-                position=chain_id,
-                leave=True,
-                colour="green",
-            )
-        else:
-            t_range = range(nsamples)
+        burn_in = nsamples * 0.2 if burn_in is None else burn_in
+        
+        # Use different seed for final alpha
 
-        # Config and efffective sample number
+        E_ana, E_num, _, accept_rate = self._sample_energy_and_optional_O(
+            wf=wf,
+            state=state,
+            MC_training_cycles=nsamples,
+            seed=seed,
+            burn_in=burn_in,
+            need_O=False,
+            num=num,
+        )
+        
         n_effective = nsamples - burn_in
-
-        state = State(state.positions, state.logp, 0, state.delta)
-        analytical_energies = torch.empty(n_effective, dtype=torch.float64)
-        numerical_energies = torch.empty(n_effective, dtype=torch.float64)
-
         
-        for i in t_range:
-            
-            metropolis_state = self.step(wf, state, seed) #
-            r_new = metropolis_state.positions
+        # compute mean energies 
+        mean_ana_energy = E_ana.mean().item()
+        mean_num_energy = E_num.mean().item() if num else mean_ana_energy
 
-            if i >= burn_in:
-        
-                if num == True:
-                    num_energy, ana_energy = self.hamiltonian.local_energy(wf, r_new, num) #calculate num, ana energies
-                    numerical_energies[i-burn_in] = num_energy.detach()
-                else:
-                    ana_energy= self.hamiltonian.local_energy(wf, r_new) #calculate num, ana energies
-                    numerical_energies[i-burn_in] = 0
+        e = E_ana.detach().cpu().numpy()
+        print("mean:", e.mean())
+        print("std:", e.std(ddof=1))
+        print("min:", e.min(), "max:", e.max())
+        print("count E < -20:", np.sum(e < -20))
+        print("count E < -50:", np.sum(e < -50))
+        print(np.percentile(e, [0.1, 1, 5, 50, 95, 99, 99.9]))
+        print(np.max(E_ana.cpu().numpy()), np.min(E_ana.cpu().numpy()))
 
-                analytical_energies[i-burn_in] = ana_energy.detach()
-            state = metropolis_state # update the state for next iteration
-
-
-        if self._logger is not None:
-            t_range.clear()
-
-        print(analytical_energies.dtype)
-        
         sample_results = {
             "chain_id": chain_id,
-            "energy_numerical":  torch.mean(numerical_energies).item(),  # calculate mean numerical energy 
-            "energy_analytical": torch.mean(analytical_energies).item(),  # calculate mean analytical energy
-            "std_error": ( analytical_energies.std(unbiased=True) 
-                        / torch.sqrt(torch.tensor(n_effective, dtype=analytical_energies.dtype))).item(),
-            "variance": analytical_energies.var(unbiased=False).item(),
+            "energy_numerical":  mean_num_energy,  # calculate mean numerical energy 
+            "energy_analytical": mean_ana_energy,  # calculate mean analytical energy
+            "std_error": ( E_ana.std(unbiased=True) 
+                        / torch.sqrt(torch.tensor(n_effective, dtype=E_ana.dtype))).item(),
+            "variance": E_ana.var(unbiased=True).item(),
             "scale": self.scale, 
             "effective samples": n_effective,
             "MC cycles": nsamples,
             "alpha": wf.alpha.item(), # get the alpha value from the wave function
+            "accept rate": accept_rate,
         }
     
         return sample_results

src/simulation_scripts/config.py
@@ -5,18 +5,20 @@ output_filename = "../data/vmc_playground.csv"
 
 nparticles = 10
 dim = 3
-nsamples = int(10_000)  # 2**18 = 262144
+nsamples = int(100_000)  # 2**18 = 262144
 nchains = 1 # number of Markov chains. When you parallelize, you can set this to the number of cores. Note you will have to implement this yourself.
 eta = 0.01
 training_cycles = 10_000  # this is cycles for the ansatz
 mcmc_alg = "metropolis" # "metropolis" or "langevin"
-scale = 0.9
+scale = 0.2     # scale for the new proposed position in metropolis algorithm
 backend = "torch"
-optimizer = "gd"
+optimizer = "gd" # "gd" for gradient descent, "adam" for Adam optimizer
 batch_size = 200
 detailed = True
+num = False
 wf_type = "vmc" 
 seed = 142
-burn_in = training_cycles * 0.2 # number of initial samples to discard as burn-in when training
+final_sampling_seed = 999
+burn_in = training_cycles * 0.2 # number of initial samples to discard as burn-in when training 10-20%
 alpha_array = np.linspace(0.1, 0.9, 11) # array of alpha values to train on
-a = 0.1     # Jastrow factor strength, set to 0 for no interactions
+a = 0.01     # Jastrow factor strength, set to 0 for no interactions

src/simulation_scripts/vmc_playground.py
@@ -47,12 +47,16 @@ system.train(
     MC_training_cycles=config.training_cycles,
     alpha_array=alpha_array,
     burn_in=config.burn_in,
+    num=config.num
 )
 
-
-# now we get the results or do whatever we want with them
-results = system.sample(config.nsamples, nchains=config.nchains, seed=config.seed)
+# make initial state for final sampling 
+system._make_initial_state() 
+results = system.sample(config.nsamples, nchains=config.nchains, seed=config.final_sampling_seed)
+print(system._scale)
 print(results)
+
+
 # display the results
 
 energies = np.array(system.mean_ana_energies)  # or mean_num_energies