41f8836b1dee07bbd6a5e180624c7051957c82bb
OzzyTheFuzzy <oskarfa@uio.no>
2026-03-15 18:54:54 +0100

    added blocking analyis for the std and var

---
+++ src/qs/functions/statistical_analysis.py
@@ -0,0 +1,61 @@
+#statistical_analysis.py
+import numpy as np
+
+
+def blocking(E_array, B):
+    """
+    fimctopion that performs the blocking method on a given array of
+    E: the array of values to be analyzed
+    B: the size of the blocks
+    """
+
+    N=len(E_array)
+    n = N//B              #number of blocks
+
+    if n < 2:
+        raise ValueError("Too few blocks to estimate variance.")
+
+    E_trim = E_array[:n * B]    #trim the array to be a multiple of B
+    blocks = E_trim.reshape(n, B)
+
+    #calculate the block averages and then finding the mean of these averages
+    block_means = np.mean(blocks, axis=1)
+    mean = np.mean(block_means)
+
+    # calculating the variance and the error of the mean
+    variance = np.sum((block_means - mean) ** 2) / (n - 1)
+    error = np.sqrt(variance / n)
+    
+    return variance, error, block_means
+
+
+  
+def blocking_error(E_array):
+    """
+    Function to calculate the standard error of the mean from the block averages
+    E_block: array of block averages
+    """
+    variance_array, error_array = [], []
+    idx=0
+    N=len(E_array)
+    n=0; k=0
+    B_list=[]
+    n_list=[]
+    while True:  
+        B = 2**k
+        n = N//B 
+
+        if n < 20:
+            break
+        B_list.append(B)
+        n_list.append(n)
+        
+        variance, error, block_means = blocking(E_array, B)
+
+        error_array.append(error)
+        variance_array.append(variance)
+
+        idx+=1
+        k+=1
+
+    return variance_array, error_array, B_list, n_list
