code works but with interactions the gradient descent is pointing towards 0.497. Not sure if that is correct

src/qs/functions/vmc_and_exact_energy.py
@@ -28,22 +28,20 @@ def find_energy_vmc(dim, nparticles, config=config, scale=config.scale):
         config.wf_type,
         nparticles,
         dim,
-        config.a
+        config.a,
+        config.beta
     )
 
     # choose the sampler algorithm and scale
-    system.set_sampler(mcmc_alg=config.mcmc_alg, scale=scale)
+    system.set_sampler(mcmc_alg=config.mcmc_alg, scale=scale, obd=False)
 
     # choose the hamiltonian
-    system.set_hamiltonian(type_="ho", int_type="Coulomb")
-
-    # scale learningrate with # of particles
-    eta=config.eta/(np.sqrt(nparticles))
+    system.set_hamiltonian(type_="ho", int_type="Coulomb", omega_ho=config.omega, omega_z=config.omega_z)
 
     # choose the optimizer, learning rate, and other properties depending on the optimizer
     system.set_optimizer(
         optimizer=config.optimizer,
-        eta=eta,
+        eta=config.eta,
     )
     
     # train the system and find the best alpha (scale adapts per alpha internally)
@@ -65,7 +63,7 @@ def vmc_vs_exact(name_of_file = "../../data/vmc_results_test.txt"):
     alpha_array = config.alpha_array
     dimensions = config.dimensions
     nparticles_array = config.nparticles_array
-    omega = config.omega_ho
+    omega = config.omega
 
     energies_vmc = np.zeros((len(dimensions), len(nparticles_array))) # array to store the VMC energies
     energies_exact = np.zeros((len(dimensions), len(nparticles_array))) # array to store the exact energies

src/qs/quantum_state.py
@@ -101,7 +101,7 @@ class QS:
             positions = 0.1 * torch.randn(self._N, self._dim, dtype=torch.float64)
 
             # safeguard for the hard core condition
-            if self.a != 0.0:
+            if self.a != 0.0 or self._N > 1:
                 r_ij_abs, _ = self.wf.wf.distance_and_distance_vec(positions)
                 iu = torch.triu_indices(self._N, self._N, offset=1)
                 rij = r_ij_abs[iu[0], iu[1]]
@@ -162,7 +162,7 @@ class QS:
         self.alpha_array_tested =[]
         self.accept_rate_array = []
 
-        if alpha_0 is not None:
+        if alpha_0 !=0.0 and alpha_0 is not None:
             alpha = torch.tensor(alpha_0, dtype=torch.float64)
             alpha_array_tested = []
             alpha_array_tested.append(alpha_0)
@@ -185,7 +185,7 @@ class QS:
             print("omega_ho:", self.hamiltonian.omega_ho, "omega_z:", self.hamiltonian.omega_z)
             
             # create tensor for current alpha and retrieve alpha value
-            if alpha_0 is None:
+            if alpha_0 == 0.0:
                 alpha = alpha_i
                 idx = np.where(alpha_array == alpha_i)[0].item()
                 a_tensor = torch.tensor(float(alpha), dtype=torch.float64)
@@ -211,6 +211,7 @@ class QS:
             
             # call sample function from sampler class
             if num:
+                print('num')
                 E_ana, E_num, O, accept_rate, t_ana_tot, t_num_tot = self.sampler._sample_energy_and_optional_O(
                 self.wf, state,
                 MC_training_cycles, seed=self._seed, 
@@ -241,7 +242,7 @@ class QS:
                 f"mean_E_ana={mean_ana_energy:.6f}, scale={self.sampler.scale:.4f}")
             
             # if gd is activated compute next alpha
-            if alpha_0 is not None:
+            if alpha_0 != 0.0 and alpha_0 is not None:
                 # Compute the gradient dE/d alpha and the mean analytical energy
                 dE_dalpha, _ = self.hamiltonian.compute_gradient(O, E_ana)
                 

src/simulation_scripts/config_steepest_descent_vmc.py
@@ -15,7 +15,7 @@ omega_z           = beta     # omexga_z=beta for elliptical trap, omega_z=0.0 fo
 nsamples          = int(2**15)   # samples for the final calculation of the energy after training
 final_burn_in     = int(nsamples // 10)
 nchains           = 1
-training_cycles   = 500000 # number of training cycles for each alpha
+training_cycles   = 250000 # number of training cycles for each alpha
 num_iterations    = 30     # number of optimization iterations
 mcmc_alg          = "langevin"  # either metropolis or langevin 
 backend           = "torch"
@@ -23,24 +23,24 @@ batch_size        = 200
 detailed          = True
 final_sampling_seed = 999
 seed                = 42
-burn_in             = training_cycles // 10  # number of burn-in samples to discard
+burn_in             = int(training_cycles // 10) * 2  # number of burn-in samples to discard
 scale               = 0.4   # scale for the MCMC proposal distribution
 num                 = False
-alpha_array         = np.linspace(0.4, 0.6, 11)  # array of alphas (not used in GD)
+alpha_array         = np.linspace(0.1, 0.9, 11)  # array of alphas (not used in GD)
 
 """ Optimizer """
 optimizer         = "gd"   # either "gd" or "adam"
-eta               = 0.0075   # learning rate for steepest descent optimizer 0.01 for 100 particles, 0.0075 for 500 particles
-alpha_0           = 0.4
+eta               = 0.005   # learning rate for steepest descent optimizer 0.01 for 100 particles, 0.0075 for 500 particles
+alpha_0           = 0.4    # put None for using jumps and not gd
 
 """ Interaction parameters """
-a         = 0.0043    # Jastrow factor strength (0 = no interactions) 0.0043  
+a         = 0.0043   # Jastrow factor strength (0.0 = no interactions) 0.0043 for standard
 
 """write to file"""
-write_to_file          = True    # True if you want to write energies to file for final sampling (used for blocking)
-write_to_file_training = True   # True if you want to write energies vs alpha during training to file
-name_of_file           = f"_N{nparticles}_d{dim}_MC{nsamples}_with_interactions.txt"  #  name of txt file for energies of last sample
-filename               = f'{alpha_0}_{optimizer}_N{nparticles}_d{dim}_MC{training_cycles}_with_interaction.txt' # name of txt file for energies vs alpha during training
+write_to_file          = False    # True if you want to write energies to file for final sampling (used for blocking)
+write_to_file_training = False   # True if you want to write energies vs alpha during training to file
+name_of_file           = f"a={a}_N{nparticles}_d{dim}_MC{nsamples}_{alpha_0}_with_interactions.txt"  #  name of txt file for energies of last sample
+filename               = f'a={a}_{alpha_0}_{optimizer}_N{nparticles}_d{dim}_MC{training_cycles}_with_interaction.txt' # name of txt file for energies vs alpha during training
 
 
 

src/simulation_scripts/config_vmc_vs_exact.py
@@ -9,8 +9,8 @@ nparticles_array = np.array([1, 10, 100, 500])
 dimensions = np.array([1, 2, 3])
 wf_type = "vmc"
 beta = 1               # for elliptical trap set beta != 1, for spherical set beta = 1
-omega_ho = 1.0
-omega_z = 1.0          # for elliptical trap set omega_z = beta, for spherical set omega_z = 1.0
+omega = 1.0
+omega_z = None          # for elliptical trap set omega_z = beta, for spherical set omega_z = 1.0
 
 """ Monte Carlo parameters """
 nsamples = int(2**20)  # samples for the final calculation of the energy after training

src/simulation_scripts/steepest_descent_vmc.py
@@ -34,7 +34,7 @@ if config.write_to_file:
 print(alphas, energies)
 iterations = np.linspace(0,len(alphas)-1, len(alphas))
 
-plt.scatter(iterations, alphas, label="alpha values")
+plt.scatter(alphas, energies, label="alpha values")
 plt.xlabel("Iteration")
 plt.ylabel("Alpha")
 plt.title(f"Updated alpha using steepest descent for {nparticles} particles in {dim}D")

src/simulation_scripts/vmc_playground.py
@@ -17,7 +17,7 @@ sys.path.insert(0, str(src_path))
 sys.path.insert(0, str(src_path / "simulation_scripts"))
 from qs.functions.write_to_file import write_to_file
 from qs.functions.onebody_density import plot_column_density
-
+from qs.functions import vmc_and_exact_energy as vmc_and_exact_energy
 
 from qs import quantum_state
 import config
@@ -29,41 +29,8 @@ jax.config.update("jax_platform_name", "cpu")
 
 def train_and_sample_obd():
     # set up the system with its backend and level of logging, seed, and other general properties depending on how you want to run it
-    system = quantum_state.QS(
-        backend=config.backend,
-        log=True,
-        logger_level="INFO",
-        seed=config.seed,
-    )
-
-    # set up the wave function with some of its properties 
-    system.set_wf(
-        config.wf_type,
-        config.nparticles,
-        config.dim,
-        config.a,
-    )
-
-    system.set_sampler(mcmc_alg=config.mcmc_alg, scale=config.scale, obd=False)
-
-    # choose the hamiltonian
-    system.set_hamiltonian(type_="ho", int_type="Coulomb", omega_ho=1.0, omega_z=config.omega_z)
-
-    # choose the optimizer, learning rate, and other properties depending on the optimizer
-    system.set_optimizer(
-        optimizer=config.optimizer,
-        eta=config.eta,
-    )
-
-    # train the system, meaning we find the optimal variational parameters for the wave function
-    system.train(MC_training_cycles=config.training_cycles, 
-                alpha_array=config.alpha_array, 
-                burn_in=config.burn_in, 
-                num_iterations=config.iterations, 
-                alpha_0=config.alpha_0, 
-                num=config.num)
-
-
+    system = vmc_and_exact_energy.find_energy_vmc(config.dim, config.nparticles, config, config.scale)
+    
     # define r_max and reset the sampler with the onebody density settings for final sampling
     r_max  =2 * torch.sqrt(1 / (2 * system.wf.alpha)) if config.r_max is None else config.r_max
     system.set_sampler(mcmc_alg=config.mcmc_alg, scale=config.scale, obd=config.obd, n_bins=config.n_bins, r_max=r_max)