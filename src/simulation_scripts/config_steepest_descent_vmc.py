918d377c6e5431deb754d0b25f744392ce18ebae
OzzyTheFuzzy <oskarfa@uio.no>
2026-03-23 11:23:44 +0100

    the gd works for 3 dims with 100 particles. Get convergence right away for scale=0.4, 500k MC cycles, eta=0.01, alpha_0=0.4

--- src/simulation_scripts/config_steepest_descent_vmc.py
+++ src/simulation_scripts/config_steepest_descent_vmc.py
@@ -5,8 +5,8 @@ output_filename = "../../data/vmc_vs_exact_test.txt"
 
 """ System parameters """
 dim               = 3
-nparticles        = 10
-omega             = 1.0   # harmonic oscillator 
+nparticles        = 100
+omega             = 1.0   # harmonic oscillator frequency
 wf_type           = "vmc"
 beta              = 1.0     #beta = 2.82843 for elliptical trap, beta = 1.0 for spherical trap
 omega_z           = 0.0     # omexga_z=beta for elliptical trap, omega_z=0.0 for spherical trap
@@ -15,27 +15,35 @@ omega_z           = 0.0     # omexga_z=beta for elliptical trap, omega_z=0.0 for
 nsamples          = int(2**15)   # samples for the final calculation of the energy after training
 final_burn_in     = int(nsamples // 10)
 nchains           = 1
-eta               = 0.04   # learning rate for steepest descent optimizer
 training_cycles   = 500000 # number of training cycles for each alpha
 num_iterations    = 30     # number of optimization iterations
 mcmc_alg          = "langevin"  # either metropolis or langevin 
 backend           = "torch"
 batch_size        = 200
 detailed          = True
-
 final_sampling_seed = 999
 seed                = 42
 burn_in             = training_cycles // 10  # number of burn-in samples to discard
 scale               = 0.4   # scale for the MCMC proposal distribution
 num                 = False
 alpha_array         = np.linspace(0.4, 0.6, 11)  # array of alphas (not used in GD)
-alpha_0             = 0.4
 
 """ Optimizer """
 optimizer         = "gd"   # either "gd" or "adam"
+eta               = 0.01   # learning rate for steepest descent optimizer
+alpha_0             = 0.4
 
 """ Interaction parameters """
 a         = 0.0    # Jastrow factor strength (0 = no interactions) 0.0043  
+
+"""write to file"""
+write_to_file          = True    # True if you want to write energies to file for final sampling (used for blocking)
+write_to_file_training = True    # True if you want to write energies vs alpha during training to file
+name_of_file           = f"_N{nparticles}_d{dim}_MC{nsamples}.txt"  #  name of txt file for energies of last sample
+filename               = f'{alpha_0}_{optimizer}_N{nparticles}_d{dim}_MC{training_cycles}.txt' # name of txt file for energies vs alpha during training
+
+
+
 
 
 
--- src/simulation_scripts/steepest_descent_vmc.py
+++ src/simulation_scripts/steepest_descent_vmc.py
@@ -10,13 +10,20 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 from qs.functions import vmc_and_exact_energy as vmc_and_exact_energy
 from simulation_scripts import config_steepest_descent_vmc as config_steep
 
-dim=config_steep.dim; nparticles=config_steep.nparticles; alpha_array = config_steep.alpha_array
+project_root = Path(__file__).resolve().parents[2]
 
-results, system = vmc_and_exact_energy.find_energy_vmc(dim, nparticles, config_steep, config_steep.scale)
-print(system.alpha_array, system.mean_ana_energies)
-plt.scatter(system.alpha_array, system.mean_ana_energies)
-plt.xlabel("alpha")
-plt.ylabel("Energy")
-plt.title(f"E(alpha) for {config_steep.num_iterations} alphas")
-#plt.grid("True")
+dim = config_steep.dim; nparticles = config_steep.nparticles; alpha_array = config_steep.alpha_array
+
+system = vmc_and_exact_energy.find_energy_vmc(dim, nparticles, config_steep, config_steep.scale)
+
+print(system.alpha_array_tested, system.mean_ana_energies)
+iterations = np.linspace(0,len(system.alpha_array_tested)-1, len(system.alpha_array_tested))
+
+plt.scatter(iterations, system.alpha_array_tested, label="alpha values")
+plt.xlabel("Iteration")
+plt.ylabel("Alpha")
+plt.title(f"Updated alpha using steepest descent for {nparticles} particles in {dim}D")
+plt.grid("True")
+
+plt.savefig(project_root / "figures" / f"iteration_alpha_{config_steep.alpha_0}.pdf", dpi=300)
 plt.show()
