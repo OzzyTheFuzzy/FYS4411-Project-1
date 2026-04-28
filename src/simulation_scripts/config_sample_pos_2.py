d6e561484f2a7793ed2a662b64d39f5182a1cfe9
OzzyTheFuzzy <oskarfa@uio.no>
2026-04-28 16:54:53 +0200

    changes to samples pos for pj2

--- src/simulation_scripts/config_sample_pos_2.py
+++ src/simulation_scripts/config_sample_pos_2.py
@@ -4,36 +4,36 @@ import numpy as np
 
 """ System parameters"""
 nparticles = 100
-dim        = 1
+dim        = 3 
 wf_type    = "vmc" 
-beta       = 1  #2.82843    # for wavefunction with eliptical trap set beta not 0, for spherical trap set beta = 1
-omega_z    = 1.0    # for elliptical trap set omega_z = beta, for spherical trap set omega_z =1.0
+beta       = 2.82843    # for wavefunction with eliptical trap set beta not 0, for spherical trap set beta = 1
+omega_z    = beta    # for elliptical trap set omega_z = beta, for spherical trap set omega_z =1.0
 omega      = 1.0  # harmonic oscillator frequency
 
 """ Monte Carlo parameters"""
-training_cycles = 50000       # this is cycles for training
+training_cycles = 1000       # this is cycles for training
 mcmc_alg        = "langevin" # "metropolis" or "langevin"
-scale           = 0.5        # scale for the new proposed position in metropolis algorithm (metropolis 0.2, langevin 0.5)
+scale          =  0.5        # scale for the new proposed position in metropolis algorithm (metropolis 0.2, langevin 0.5)
 backend         = "torch"
 batch_size      = 200
 detailed        = True
 num             = False      # Set num=True to calculate the second derivatives with numerical derivation
 nsamples        = int(100000)  # 2**18 = 262144
-seed            = 142
+seed            = 24
 final_burn_in   = int(nsamples//10)*2
 
 final_sampling_seed = 999
 burn_in             = int(training_cycles//10 * 2) # number of initial samples to discard as burn-in when training 10-20%
-alpha_array         = np.linspace(0.1, 0.9, 21) # array of alpha values to train on
+alpha_array         = np.array([0.5]) # array of alpha values to train on
 nchains             = 1 # number of Markov chains
-write_to_file       = True    # True if you want to write energies to file
-write_to_file_training= True    # True if you want to write energies vs alpha during training to file
+write_to_file       = False   # True if you want to write energies to file
+write_to_file_training= False    # True if you want to write energies vs alpha during training to file
 name_of_file        = f"testing{nparticles}_d{dim}"  #  name of txt file for energies of last sample
 filename            = f'energy_vs_alpha_{mcmc_alg}.txt' # name of txt file for energies vs alpha during training
 
 """ Gradient descent parameters"""
 optimizer = "gd"      # "gd" for gradient descent, "adam" for Adam optimizer
-iterations  = 30      # different alphas tested
+num_iterations  = 30      # different alphas tested
 alpha_0     = None    # starting alpha ( set to None for no optimizer) ()
 eta         = 0.001   # learning rate for alpha updates (0.01 is good choice)
 need_O = False
---
+++ src/simulation_scripts/sample_pos_2.py
@@ -0,0 +1,46 @@
+#sample_pos_2.py
+
+# For training and writing to file. Also for making plots for E(alpha) vs alpha
+import sys
+import matplotlib.pyplot as plt
+from pathlib import Path
+#sys.path.append("/Users/oskarfausko/Desktop/compfys 2/Project1/project1/FYS4411-Template/src/") # append yout path to the src folder
+# Project paths
+project_root = Path(__file__).resolve().parents[2]
+src_path = project_root / "src"
+data_dir = project_root / "positions_energy_data"
+
+sys.path.insert(0, str(src_path))
+sys.path.insert(0, str(src_path / "simulation_scripts"))
+from qs.functions.write_to_file import write_to_file
+
+import jax
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+from qs.functions import vmc_and_exact_energy as vmc_and_exact_energy
+import config_sample_pos_2 as config  
+
+
+jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_platform_name", "cpu")
+
+
+def train_and_sample():
+
+    system = vmc_and_exact_energy.find_energy_vmc(config.dim, config.nparticles, config, config.scale)
+
+    # make initial state for final sampling and run final sampling
+    system._make_initial_state()
+
+    results = system.sample(config.nsamples, config.final_burn_in, nchains=config.nchains, seed=24, 
+                            num=config.num, write_to_file=config.write_to_file, name_of_file=config.name_of_file)
+    return system, results
+
+system, results = train_and_sample()
+
+data = np.load(data_dir / "r_all_E_N1_d3.npz")
+r_all = data["r_all"]
+E_ana = data["E"]
+
+print(r_all.shape)
